20651970f1c2aaac6ca2d60e7ec898d434c0999b
Tanesan <54165015+Tanesan@users.noreply.github.com>
2020-08-03 20:47:49 +0900

    setting

--- gpsgame/src/settings.js
+++ gpsgame/src/settings.js
@@ -31,11 +31,8 @@ import SecurityIcon from '@material-ui/icons/Security';
 import AccountBalanceIcon from '@material-ui/icons/AccountBalance';
 import { spacing } from '@material-ui/system';
 import PropTypes from 'prop-types';
-import { makeStyles } from '@material-ui/core/styles';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
-import Typography from '@material-ui/core/Typography';
-import Box from '@material-ui/core/Box';
 const drawerWidth = 240;
 
 const useStyles = makeStyles((theme) => ({
@@ -98,12 +95,57 @@ const useStyles = makeStyles((theme) => ({
     flexGrow: 1,
     padding: theme.spacing(3),
   },
+  root: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.paper,
+    display: 'flex',
+    height: 224,
+  },
+  tabs: {
+    borderRight: `1px solid ${theme.palette.divider}`,
+  },
 }));
 
-export default function Settings() {
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`vertical-tabpanel-${index}`}
+      aria-labelledby={`vertical-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box p={3}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.any.isRequired,
+  value: PropTypes.any.isRequired,
+};
+
+function a11yProps(index) {
+  return {
+    id: `vertical-tab-${index}`,
+    'aria-controls': `vertical-tabpanel-${index}`,
+  };
+}
+
+
+
+export default function Settings(props) {
   const classes = useStyles();
   const theme = useTheme();
   const [open, setOpen] = React.useState(false);
+  
 
   const handleDrawerOpen = () => {
     setOpen(true);
@@ -111,6 +153,11 @@ export default function Settings() {
 
   const handleDrawerClose = () => {
     setOpen(false);
+  };
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
   };
 
   return (
@@ -120,6 +167,46 @@ export default function Settings() {
         <Typography paragraph>
           Settings
         </Typography>
+        <div className={classes.root}>
+      <Tabs
+        orientation="vertical"
+        variant="scrollable"
+        value={value}
+        onChange={handleChange}
+        aria-label="Vertical tabs example"
+        className={classes.tabs}
+      >
+        <Tab label="Account" {...a11yProps(0)} />
+        <Tab label="Password" {...a11yProps(1)} />
+        <Tab label="Privacy and safety" {...a11yProps(2)} />
+        <Tab label="Email notifications" {...a11yProps(3)} />
+        <Tab label="Web notifications" {...a11yProps(4)} />
+        <Tab label="Your data" {...a11yProps(5)} />
+        <Tab label="Delete account" {...a11yProps(6)} />
+      </Tabs>
+      <TabPanel value={value} index={0}>
+        Public info
+
+      </TabPanel>
+      <TabPanel value={value} index={1}>
+        Password
+      </TabPanel>
+      <TabPanel value={value} index={2}>
+      Privacy and safety
+      </TabPanel>
+      <TabPanel value={value} index={3}>
+      Email notifications
+      </TabPanel>
+      <TabPanel value={value} index={4}>
+      Web notifications
+      </TabPanel>
+      <TabPanel value={value} index={5}>
+      Your data
+      </TabPanel>
+      <TabPanel value={value} index={6}>
+      Delete account
+      </TabPanel>
+    </div>
       </main>
   );
 }
